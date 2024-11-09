fb9d5feff36814e70dc287237db4dc6d7de31078
will-3211 <146374113+will-3211@users.noreply.github.com>
2024-11-09 15:14:32 -0600

    Updated 11/9 v2

--- main.c
+++ main.c
@@ -50,6 +50,5 @@ int main(int argc, char *argv[]){
 
 
 
-
 return 0;
 }
--- main.h
+++ main.h
@@ -1,8 +1,41 @@
 #include <math.h>
 
 
-int calcCacheSize(int x, int y, int z){
-    int tmp1 = x * 2;
+void inputOutput(){
+    printf("Cache Simulator - CS 3853 - Group 12\n\n");
+    
+    printf("Trace File(s):\n"
+            "\t\t%s\n"
+            "\t\t%s\n"
+            "\t\t%s\n", NULL, NULL, NULL);
 
-    return tmp1;
+    printf("***** Cache Input Parameters *****\n\n"
+            "Cache Size:                    %d\n"
+            "Block Size:                    %d\n"
+            "Associativity:                 %d\n"
+            "Replacement Policy:            %s\n"
+            "Physical Memory: 1024          %d\n"
+            "Percent Memory Used by System: %d\n"
+            "Instructions / Time Slice:     %d\n");
+
+}
+
+void calculatedOutput(){
+    int cacheSize = calcCacheSize(1, 2, 3);
+    printf("***** Cache Calculated Values *****\n\n"
+            "Total # Blocks:                \n"
+            "Tag Size:                      \n"
+            "Index Size:                    \n"
+            "Total # Rows:                  \n"
+            "Overhead Size:                 \n"
+            "Implementation Memory Size:    \n"
+            "Cost:                          \n");
+}
+
+void pmCalculatedOutput(){
+    printf("***** Physical Memory Calculated Values *****\n\n"
+            "Number of Physical Pages:      \n"
+            "Number of Pages for System:    \n"
+            "Size of Page Table Entry:      \n"
+            "Total RAM for Page Table(s):   \n");
 }

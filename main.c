2701153c7c0987333f8a9cf4b5a2fa496e253e30
will-3211 <146374113+will-3211@users.noreply.github.com>
2024-11-09 18:36:42 -0600

    Commit 11/9 v3

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[]){
                         "–b <block size>            [ 8 bytes to 64 bytes ]\n"
                         "–a <associativity>         [ 1, 2, 4, 8, 16 ]\n"
                         "–r <replacement policy>    [ RR ]\n"
+                        "–p <physical memory - MB> [ 1 MB to 4 GB ]\n"
                         "-u <percentage phys mem used> [ 0 to 100 ]\n"
                         "-n <Instr / Time Slice [ 0 to -1 ] -1 = max]\n"
                         "–f <trace file name> [ name of text file with the trace ]\n" ,argv[0]);
@@ -20,28 +21,29 @@ int main(int argc, char *argv[]){
     int cacheSize = atoi(argv[1]);
     int blockSize = atoi(argv[2]);
     int associativity  = atoi(argv[3]);
-    int percentageMem = atoi(argv[5]);
-    int intructionTimeSlice = atoi(argv[6]);
+    int physicalMem = atoi (argv[4]);
+    int percentageMem = atoi(argv[6]);
+    int intructionTimeSlice = atoi(argv[7]);
 
     char replacementPolicy[30];
-    strncpy(replacementPolicy, argv[4], sizeof(replacementPolicy) - 1);
+    strncpy(replacementPolicy, argv[5], sizeof(replacementPolicy) - 1);
     replacementPolicy[sizeof(replacementPolicy) - 1] = '\0'; //Null-terminate
     
 
     char traceFile1[30];
-    strncpy(traceFile1, argv[7], sizeof(traceFile1) - 1);
+    strncpy(traceFile1, argv[8], sizeof(traceFile1) - 1);
     traceFile1[sizeof(traceFile1) - 1] = '\0';  // Null-terminate
 
     
     char traceFile2[30];
-    if (argv[8] != NULL) {
+    if (argv[9] != NULL) {
         //char traceFile2[30];
         strncpy(traceFile2, argv[8], sizeof(traceFile2) - 1);
         traceFile2[sizeof(traceFile2) - 1] = '\0';  // Null-terminate
     }
 
     char traceFile3[30];
-    if (argv[9] != NULL) {
+    if (argv[10] != NULL) {
         //char traceFile3[30];
         strncpy(traceFile3, argv[9], sizeof(traceFile3) - 1);
         traceFile3[sizeof(traceFile3) - 1] = '\0';  // Null-terminate
--- main.h
+++ main.h
@@ -1,27 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
+//Calculations
+int blockReturn(int cache, int block){
+        return cache/block;
+}
+int tagReturn(int block){
+        return log2(block);
+}
+int indexReturn(int numBlocks, int associativity){
+        int numSets = numBlocks / associativity;
+        return log2(numSets);
+}
+int numRows(int numBlocks, int associativity){
+        return numBlocks / associativity;
+}
+int overheadSize(){
+        //Fill in
+}
+double cost(){
+        //Fill in
+}
+int numPhysPages(){
+        //Fill in
+}
+int numPagesSys(){
+        //Fill in
+}
+int sizePTE(){
+        //Fill in
+}
+int totalRAMPT(){
+        //Fill in
+}
 
-void inputOutput(){
+
+//Console Outputs
+void inputOutput(int cache, int block, int associativity, char replacementPolicy[],
+                         int physicalMem, int percentageMem, int intructionTimeSlice,
+                         char traceFile1[], char traceFile2[], char traceFile3[]){
     printf("Cache Simulator - CS 3853 - Group 12\n\n");
     
     printf("Trace File(s):\n"
             "\t\t%s\n"
             "\t\t%s\n"
-            "\t\t%s\n", NULL, NULL, NULL);
+            "\t\t%s\n", traceFile1, traceFile2, traceFile3);
+
 
     printf("***** Cache Input Parameters *****\n\n"
             "Cache Size:                    %d\n"
             "Block Size:                    %d\n"
             "Associativity:                 %d\n"
             "Replacement Policy:            %s\n"
-            "Physical Memory: 1024          %d\n"
+            "Physical Memory:               %d\n"
             "Percent Memory Used by System: %d\n"
-            "Instructions / Time Slice:     %d\n");
+            "Instructions / Time Slice:     %d\n"
+            ,cache, block, associativity, replacementPolicy, physicalMem, percentageMem, intructionTimeSlice);
 
 }
 
-void calculatedOutput(){
-    int cacheSize = calcCacheSize(1, 2, 3);
+void calculatedOutput(int cache, int block, int associativity){
     printf("***** Cache Calculated Values *****\n\n"
             "Total # Blocks:                \n"
             "Tag Size:                      \n"
@@ -29,13 +69,17 @@ void calculatedOutput(){
             "Total # Rows:                  \n"
             "Overhead Size:                 \n"
             "Implementation Memory Size:    \n"
-            "Cost:                          \n");
-}
+            "Cost:                          \n"
+            ,blockReturn(cache, block), tagReturn(block), indexReturn(blockReturn(cache, block), associativity),
+                numRows(blockReturn(cache, block), associativity), NULL, NULL, NULL);
+}               //Replace NULL with actual values
 
 void pmCalculatedOutput(){
     printf("***** Physical Memory Calculated Values *****\n\n"
             "Number of Physical Pages:      \n"
             "Number of Pages for System:    \n"
             "Size of Page Table Entry:      \n"
-            "Total RAM for Page Table(s):   \n");
+            "Total RAM for Page Table(s):   \n"
+            , NULL, NULL, NULL, NULL);
+            //Replace NULL for actual values
 }
